4a12ae602faa4b03a6f0b8a307356c982ce0814f
Rahul Kumar <49235122+rk10496@users.noreply.github.com>
2024-05-21 13:57:22 -0400

    Update MkDup.sh

--- Preprocessing/Mark Dup/MkDup.sh
+++ Preprocessing/Mark Dup/MkDup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# This script is used to assign in a single group and mark duplicate reads of BAM files.
+# This script assigns all reads in a single read group, marks duplicate reads, and recalibrates the base score.
 
 # Author=Rahul_kumar/LeeOestereich lab
+
 
 #SBATCH --job-name=Mapping_recalibration
 #SBATCH -N 1
 #SBATCH --cpus-per-task=64
-#SBATCH -t 1-00:00
+#SBATCH -t 2-00:00
  
 # Modules required
 module purge
 Picard=/ihome/crc/install/picard/2.18.12
+GATK=/ihome/crc/install/gatk/gatk-4.5.0.0
+module load gcc/8.2.0
  
 #Create a new directory
 designated_path="/bgfs/alee/LO_LAB/Personal/Rahul/Test_sample"
@@ -23,6 +26,12 @@ input_dir2="/bgfs/alee/LO_LAB/Personal/Rahul/Test_sample/5_mkdup"
 
 # Output directory
 output_dir="/bgfs/alee/LO_LAB/Personal/Rahul/Test_sample/5_mkdup"
+
+# Reference genome fasta file
+reference="/bgfs/alee/LO_LAB/Personal/Rahul/Software/Human_ref_GRCH38.p7/Human_genome_hg38_p7_chr_only.fna"
+
+# Known sites for BQSR 
+known_sites="/bgfs/alee/LO_LAB/Personal/Rahul/Software"
 
 # AddOrReplaceReadGroups, assigns all the reads in a file to a single new read-group.
 
@@ -78,7 +87,6 @@ echo "Marking duplicates completed for all samples."
 
 # BuildBamIndex, used to generate a BAM index ".bai" file.
 
-
 # Iterate over each rg_mkdp_bam file in the input directory
 for rg_mkdp_bam in "$input_dir2"/*_rg.mkdp.bam; do
 
@@ -98,3 +106,64 @@ for rg_mkdp_bam in "$input_dir2"/*_rg.mkdp.bam; do
 done
 
 echo "Indexing completed for all rg_mkdup_bam files."
+
+
+# BaseRecalibrator used to generates recalibration table for Base Quality Score Recalibration (BQSR).
+# ApplyBQSR to recalibrates the base qualities of the input reads based on the recalibration table.
+
+
+# Iterate over each rg_mkdp_bam file in the input directory
+for rg_mkdp_bam in "$input_dir"/*rg.mkdp.bam; do
+
+    # Extract sample name from the rg_mkdp_bam file name
+    sample=$(basename "$rg_mkdp_bam" rg.mkdp.bam)
+    
+    # Output recalibration table for the sample
+    recal_table="${output_dir}/${sample}.recal.table"
+    
+    # Output recalibrated BAM file
+    recal_bam="${output_dir}/${sample}.recal.bam"
+    
+    # Run BaseRecalibrator utility
+    java -jar /ihome/crc/install/gatk/gatk-4.5.0.0/gatk-package-4.5.0.0-local.jar BaseRecalibrator \
+        -I "$rg_mkdp_bam" \
+        -R "$reference" \
+        --known-sites "$known_sites"/Homo_sapiens_assembly38.known_indels.vcf.gz \
+        --known-sites "$known_sites"/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
+        -O "$recal_table"
+    
+    echo "Recalibration table created for $sample"
+    
+    # Apply BQSR utility
+    java -jar /ihome/crc/install/gatk/gatk-4.5.0.0/gatk-package-4.5.0.0-local.jar ApplyBQSR \
+        -I "$rg_mkdp_bam" \
+        -R "$reference" \
+        --bqsr-recal-file "$recal_table" \
+        -O "$recal_bam"
+    
+    echo "Recalibrated BAM file created for $sample"
+done
+
+echo "Recalibrated BAM files generated for all rg_mkdup_bam files."
+
+# BuildBamIndex, used to generate a BAM index ".bai" file.
+
+# Iterate over each recal_bam file in the input directory
+for recal_bam in "$input_dir2"/*.recal.bam; do
+
+    # Extract sample name from the recal_bam file name
+    sample=$(basename "$recal_bam" .recal.bam)
+
+    # Output rg mkdp bam bai file for the sample
+     recal_bam_bai="${output_dir}/${sample}recal.bam.bai"
+   
+    # Run BuildBamIndex utility
+    java -jar $Picard/picard.jar BuildBamIndex \
+        I="$recal_bam" \
+        O="$recal_bam_bai" \
+    
+    echo "Index file created: $recal_bam_bai"
+		
+done
+
+echo "Indexing completed for all recal_bam files."
